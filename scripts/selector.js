ddfec92f79385eaa20703aa9127e2b668bab9d7e
Jennifer M <156385936+JenniferM04@users.noreply.github.com>
2024-02-02 19:08:04 -0500

    Highlight and css Selectors

--- scripts/selector.js
+++ scripts/selector.js
@@ -1,4 +1,36 @@
+/**
+ * Jennifer Murphy
+ * February 2, 2024
+ */
 
+console.log("selectors.js loaded")
+$(function()
+{
+    console.log("DOM is ready");
+});
+
+/**
+ * 
+ * @param {*} element
+ */
+function highlight(element)
+{
+    element.css('background-color', "#fcf30040")
+}
+
+function cssSelectors()
+{
+    // Updating by tag (<p></p>)
+    $('p').css('background-color', "#2a9d8f");
+
+    // Updating by class (class="red-box")
+    $('.red-box').css('background-color', "#2a9d8f");
+
+    // Updating by id (id="list")
+    $('#list').css('font-size', '40px');
+
+    $('li:odd').css('background-color', "#fcf30040");
+}
 
 
 
