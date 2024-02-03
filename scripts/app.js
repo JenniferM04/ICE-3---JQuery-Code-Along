b756cd5507d692f1fd4d42acb3a400c0a33c3082
Jennifer M <156385936+JenniferM04@users.noreply.github.com>
2024-02-02 19:12:48 -0500

    More arrays
    Creating an array with a constructor, using the push function and making a Tuesday array

--- scripts/app.js
+++ scripts/app.js
@@ -15,3 +15,15 @@ console.log(`Not Empty array: ${myArrayConstructor}`);
 let myConstructorArray = new Array(10, 20, 30, 40, 50);
 console.log(`# value array ${myConstructorArray}`);
 console.log(`# value array obj`, myArrayConstructor);
+
+// Create array with constructor
+let array2 = new Array(10);
+console.log(`New array: ${array2}`);
+
+// Add (push) 80 to array
+array2.push(80);
+console.log(`New array: ${array2}`);
+
+// Tuesday
+let tuesdayArray = new Array(10).fill("Tuesday!");
+console.log(`Class is on ${tuesdayArray}`);
