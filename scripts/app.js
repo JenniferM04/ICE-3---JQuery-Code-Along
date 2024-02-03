9e5a35054962affa66bc8fe33daf2dfeb1617a27
Jennifer M <156385936+JenniferM04@users.noreply.github.com>
2024-02-02 19:11:02 -0500

    Creating arrays
    Creating an array using constructor and creating an array with values also using constructor

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+/**
+ * Jennifer Murphy
+ * February 2, 2024
+ */
+
+let myArray = [1, 2, 3, 4, 5];
+
+// Creating array using constructor
+let myArrayConstructor = new Array();
+console.log(`Empty array: ${myArrayConstructor}`);
+myArrayConstructor.push(24);
+console.log(`Not Empty array: ${myArrayConstructor}`);
+
+// Creating array with values using constructor
+let myConstructorArray = new Array(10, 20, 30, 40, 50);
+console.log(`# value array ${myConstructorArray}`);
+console.log(`# value array obj`, myArrayConstructor);
